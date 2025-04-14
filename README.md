e144d7d5dac8c12ba8841a1f82c0bf17d8e17c04
Pramuditha <pramudithakavinda01@gmail.com>
2025-04-15 00:10:55 +0530

    add hero section

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 🩺 MediQ – A full-stack doctor appointment booking platform built with the MERN stack (MongoDB, Express.js, React, Node.js). MediQ allows patients to schedule appointments, view doctor availability, and make secure payments via Stripe.
 
-Last : Navigation 30:50
+Last : 41:00
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* ===== hero section ===== */}
+      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* ===== hero content ===== */}
+            <div>
+              <div className="lg:w-[570px]">
+                <h1>We help patient live a healthy, longer life</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
