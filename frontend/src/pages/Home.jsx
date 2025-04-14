52a7c8e8f033848720f98d082a69f301e98585e0
Pramuditha <pramudithakavinda01@gmail.com>
2025-04-15 00:27:12 +0530

    Update hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,16 @@ const Home = () => {
             {/* ===== hero content ===== */}
             <div>
               <div className="lg:w-[570px]">
-                <h1>We help patient live a healthy, longer life</h1>
+                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                  We help patient live a healthy, longer life
+                </h1>
+                <p className="text_para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Blanditiis molestias fugiat natus ipsa. Pariatur ad id
+                  perferendis ratione asperiores, ullam incidunt. Accusamus
+                  aliquam ipsa unde quo fugiat suscipit nobis nostrum!
+                </p>
+                <button className="btn">Request an Appointment</button>
               </div>
             </div>
           </div>
