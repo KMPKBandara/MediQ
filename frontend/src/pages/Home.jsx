c50303c32975e8b1f3fc9264f26986d989183cbb
Pramuditha <pramudithakavinda01@gmail.com>
2025-04-23 00:06:41 +0530

    About section completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,11 @@ import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
 import icon1 from "../assets/images/icon01.png";
+import icon2 from "../assets/images/icon02.png";
+import icon3 from "../assets/images/icon03.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
+import About from "../components/About/About";
 
 const Home = () => {
   return (
@@ -101,13 +104,59 @@ const Home = () => {
                   to="/doctors"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
-                  <BsArrowRight />
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon2} alt="" />
+              </div>
+
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Find a Location
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. One health system offers
+                  unmatched, expert health care. From the lab to the clinic.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon3} alt="" />
+              </div>
+
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Book Appointment
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. One health system offers
+                  unmatched, expert health care. From the lab to the clinic.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
               </div>
             </div>
           </div>
         </div>
       </section>
+      {/* ----- about section ----- */}
+      <About />
     </>
   );
 };
