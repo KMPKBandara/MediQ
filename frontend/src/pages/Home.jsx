8ff07ea8b38dcefc16678b601c046e07f339961a
Pramuditha <pramudithakavinda01@gmail.com>
2025-05-02 23:57:27 +0530

    Doctors section structure complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -234,6 +234,18 @@ const Home = () => {
         </div>
       </section>
       {/* ===== feature section end ===== */}
+      {/* ===== our great doctors ===== */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Great Doctors</h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert health care.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
