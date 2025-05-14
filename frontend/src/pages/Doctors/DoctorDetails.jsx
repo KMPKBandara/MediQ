8fa73443862c8d8a86fee379a325fc2f049f37fe
Pramuditha <pramudithakavinda01@gmail.com>
2025-05-14 22:49:08 +0530

    Update Doctor details

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -17,7 +17,7 @@ const DoctorDetails = () => {
                   Surgeon
                 </span>
                 <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-semibold">
-                  Muhibur Rahman
+                  Saman Kumara
                 </h3>
                 <div className="flex items-center gap-[6px]">
                   <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
