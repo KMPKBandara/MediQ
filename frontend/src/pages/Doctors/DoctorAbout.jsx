d65f7147353f1e0698f5f149f7a9a49040341d8c
Pramuditha <pramudithakavinda01@gmail.com>
2025-05-17 11:28:28 +0530

    Individual DoctorAbout Completed

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { formatDate } from "../../utils/formatDate";
 
 const DoctorAbout = () => {
   return (
@@ -27,9 +28,9 @@ const DoctorAbout = () => {
           <li className="flex  flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                23 June, 2008
+                {formatDate("12-04-2010")} - {formatDate("12-04-2016")}
               </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
+              <p className="text-[15px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
               </p>
             </div>
@@ -40,12 +41,42 @@ const DoctorAbout = () => {
           <li className="flex  flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                23 June, 2008
+                {formatDate("08-03-2010")} - {formatDate("12-04-2014")}
               </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
+              <p className="text-[15px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
               </p>
             </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formatDate("08-03-2010")} - {formatDate("12-04-2014")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formatDate("08-03-2010")} - {formatDate("12-04-2014")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
             <p className="text-[14px] leading-5 font-medium text-textColor">
               New Apollo Hospital, New York.
             </p>
--- frontend/src/utils/formateDate.js
+++
@@ -1,6 +0,0 @@
-export const formateDate = (date, config) => {
-  const defaultOptions = { day: "numaric", month: "long", year: "numaric" };
-  const options = config ? config : defaultOptions;
-
-  return new Date(date).toLocaleDateString("en-US", options);
-};
