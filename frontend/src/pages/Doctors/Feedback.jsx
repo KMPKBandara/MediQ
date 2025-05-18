06a6bf873d7702f5a08632efc3d6f169f6783675
Pramuditha <pramudithakavinda01@gmail.com>
2025-05-19 00:15:16 +0530

    Set Feedback form

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import avatar from "../../assets/images/avatar-icon.png";
+import { formatDate } from "../../utils/formatDate";
+import { AiFillStar } from "react-icons/ai";
+import FeedbackForm from "./FeedbackForm";
 
 const Feedback = () => {
+  const [showFeedbackForm, setShowFeedbackForm] = useState(false);
+
   return (
     <div>
       <div className="mb-[50px]">
@@ -17,11 +22,29 @@ const Feedback = () => {
               <h5 className="text-[16px] leading-6 text-primaryColor font-bold">
                 Ali Ahamed
               </h5>
-              <p></p>
+              <p className="text-[14px] leading-6 text-textColor">
+                {formatDate("02-14-2023")}
+              </p>
+              <p className="text_para mt-3 font-medium text-[15px]">
+                Good service, Highly recommended
+              </p>
             </div>
+          </div>
+          <div className="flex gap-1">
+            {[...Array(5).keys()].map((_, index) => (
+              <AiFillStar key={index} color="#0067FF" />
+            ))}
           </div>
         </div>
       </div>
+      {!showFeedbackForm && (
+        <div className="text-center">
+          <button className="btn" onClick={() => setShowFeedbackForm(true)}>
+            Give Feedback
+          </button>
+        </div>
+      )}
+      {showFeedbackForm && <FeedbackForm />}
     </div>
   );
 };
